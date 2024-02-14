cf442aa7cabb48199389b8bafa1c66ed3410375d
Ryan Hageman <ryan.hageman@teamsparq.com>
2024-02-14 11:20:18 -0600

    Add TreeSitter with basic config

--- init.lua
+++ init.lua
@@ -29,6 +29,7 @@ local plugins = {
     'nvim-telescope/telescope.nvim', tag = '0.1.5',
       dependencies = { 'nvim-lua/plenary.nvim' }
   },
+  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
 }
 
 local options = {}
@@ -44,3 +45,10 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+local configs = require("nvim-treesitter.configs")
+configs.setup({
+  ensure_installed = { "lua", "javascript", },
+  highlight = { enable = true },
+  indent = { enable = true },  
+})
