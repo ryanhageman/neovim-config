71eeb7869ee28c33e5e78510738ac65fdff04ae5
Ryan Hageman <ryan.hageman@teamsparq.com>
2024-02-14 11:10:50 -0600

    Add some keyboard shortcuts to Telescope

--- init.lua
+++ init.lua
@@ -40,4 +40,7 @@ require("lazy").setup(plugins, opts)
 vim.cmd.colorscheme "gruvbox-material"
 
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {}) 
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
