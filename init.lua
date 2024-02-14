ccee8a919d8ca3f802a9adfcffebea123ecf2c9e
Ryan Hageman <ryan.hageman@teamsparq.com>
2024-02-14 13:46:03 -0600

    Update Telescope Keymaps

--- init.lua
+++ init.lua
@@ -41,10 +41,10 @@ require("lazy").setup(plugins, opts)
 vim.cmd.colorscheme "gruvbox-material"
 
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {}) 
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>sf', builtin.find_files, {}) 
+vim.keymap.set('n', '<leader>sg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>sb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>sh', builtin.help_tags, {})
 
 local configs = require("nvim-treesitter.configs")
 configs.setup({
