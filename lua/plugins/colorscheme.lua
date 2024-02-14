ee4c9d507ed0094a62a55f6e608a9c4f96eb82ca
Ryan Hageman <ryan.hageman@teamsparq.com>
2024-02-14 14:54:15 -0600

    Extract into modules, add lualine

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,2 @@
+return { "sainnhe/gruvbox-material" }
+
---
+++ lua/ryan/vim-options.lua
@@ -0,0 +1,8 @@
+vim.o.expandtab = true
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+
+vim.g.mapleader = " "
+
+vim.g.python3_host_prog = "/usr/bin/python3"
