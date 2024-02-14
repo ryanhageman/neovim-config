3d2c76dea6dfeb9d66f4ce10133c5193080e14d3
Ryan Hageman <ryan.hageman@teamsparq.com>
2024-02-14 11:07:35 -0600

    Add Lazy for Package Management, Gruvbox, and Telescope with basic settings

--- init.lua
+++ init.lua
@@ -2,3 +2,42 @@ vim.o.expandtab = true
 vim.o.tabstop = 2
 vim.o.softtabstop = 2
 vim.o.shiftwidth = 2
+
+vim.g.mapleader = " "
+
+vim.g.python3_host_prog = "/usr/bin/python3"
+
+-- ** Package Management ** --
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+	{ "sainnhe/gruvbox-material" }, -- Gruvbox Theme
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.5',
+      dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+}
+
+local options = {}
+
+require("lazy").setup(plugins, opts)
+
+-- ** ** --
+
+vim.cmd.colorscheme "gruvbox-material"
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
