4f9c91c0c5f6fadd0a020d384e559c13bf93f30f
Ryan Hageman <ryan.hageman@teamsparq.com>
2024-02-19 19:42:05 -0600

    Add Python

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,21 +1,25 @@
 return {
-  "nvimtools/none-ls.nvim",
-  config = function()
-    local null_ls = require("null-ls")
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
 
-    null_ls.setup({
-      sources = {
-        -- Lua --
-        null_ls.builtins.formatting.stylua,
+		null_ls.setup({
+			sources = {
+				-- Lua --
+				null_ls.builtins.formatting.stylua,
 
-        -- Ruby --
-        null_ls.builtins.diagnostics.rubocop,
-        null_ls.builtins.formatting.rubocop,
+				-- Ruby --
+				null_ls.builtins.diagnostics.rubocop,
+				null_ls.builtins.formatting.rubocop,
 
-        -- JavaScript --
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.diagnostics.eslint_d,
-      },
-    })
-  end,
+				-- JavaScript --
+				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.diagnostics.eslint_d,
+
+				-- Python --
+				null_ls.builtins.formatting.black,
+				null_ls.builtins.diagnostics.flake8,
+			},
+		})
+	end,
 }
