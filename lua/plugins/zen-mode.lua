4128a6e7ad2830ed3b029c82fcb2f4f692f82808
Ryan Hageman <ryan.hageman@teamsparq.com>
2024-02-22 10:42:39 -0600

    Add Zen Mode

---
+++ lua/plugins/zen-mode.lua
@@ -0,0 +1,12 @@
+return {
+  "Pocco81/true-zen.nvim",
+  config = function()
+    require("true-zen").setup()
+
+    vim.keymap.set("n", "<leader>bzn", ":TZNarrow<CR>", { desc = "zen [n]arrow" })
+    vim.keymap.set("v", "<leader>bzn", ":'<,'>TZNarrow<CR>", { desc = "zen [n]arrow" })
+    vim.keymap.set("n", "<leader>bzf", ":TZFocus<CR>", { desc = "zen [f]ocus" })
+    vim.keymap.set("n", "<leader>bzm", ":TZMinimalist<CR>", { desc = "zen [m]inimalist" })
+    vim.keymap.set("n", "<leader>bza", ":TZAtaraxis<CR>", { desc = "[a]taraxis (zen mode)" })
+  end,
+}
