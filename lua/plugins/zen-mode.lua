20c042c1ee3cba13cce561fc37172968a1612513
Ryan Hageman <ryan.hageman@teamsparq.com>
2024-02-22 14:17:23 -0600

    Add Zen Mode Plugin

--- lua/plugins/zen-mode.lua
+++ lua/plugins/zen-mode.lua
@@ -1,7 +1,12 @@
 return {
   "Pocco81/true-zen.nvim",
   config = function()
-    require("true-zen").setup()
+    require("true-zen").setup({
+      integrations = {
+        twilight = true, -- enable twilight (ataraxis)
+        lualine = true, -- hide nvim-lualine (ataraxis)
+      },
+    })
 
     vim.keymap.set("n", "<leader>bzn", ":TZNarrow<CR>", { desc = "zen [n]arrow" })
     vim.keymap.set("v", "<leader>bzn", ":'<,'>TZNarrow<CR>", { desc = "zen [n]arrow" })
