f66b3b983da24888d1893a868ff56bfdde13469d
Ryan Hageman <ryan.hageman@teamsparq.com>
2024-02-23 09:49:53 -0600

    Update Alpha Config

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -62,15 +62,10 @@ return {
     dashboard.section.buttons.val = {
       -- dashboard.button("SPC j", "󰈚   Restore Session", ":SessionRestore<cr>"),
       dashboard.button("p", "󰸕   Projects", ":Telescope projects<CR>"),
-      dashboard.button("e", "   New file", ":ene <BAR> startinsert <CR>"),
-      dashboard.button("f", "   Find file", ":Telescope find_files<CR>"),
-      dashboard.button("g", "󰱼   Find word", ":Telescope live_grep<CR>"),
       dashboard.button("r", "   Recent", ":Telescope oldfiles<CR>"),
+      dashboard.button("f", "   Find file", ":Telescope find_files<CR>"),
       dashboard.button("n", "󰂺   Notebook", ":ObsidianToday<CR>"),
       dashboard.button("c", "   Config", ":e $MYVIMRC <CR>"),
-      dashboard.button("m", "󱌣   Mason", ":Mason<CR>"),
-      dashboard.button("l", "󰒲   Lazy", ":Lazy<CR>"),
-      dashboard.button("u", "󰂖   Update plugins", "<cmd>lua require('lazy').sync()<CR>"),
       dashboard.button("q", "󰍃   Quit NVIM", ":qa<CR>"),
     }
 
