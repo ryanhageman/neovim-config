17d98a95517cd39ce92841cf5eb5db57205d97e2
Ryan Hageman <ryan.hageman@teamsparq.com>
2024-02-27 10:37:19 -0600

    Update NeoClip settings
    Persist the list

--- lua/plugins/neo-clip.lua
+++ lua/plugins/neo-clip.lua
@@ -2,10 +2,12 @@ return {
 	"AckslD/nvim-neoclip.lua",
 	dependencies = {
 		{ "nvim-telescope/telescope.nvim" },
+		{ "kkharji/sqlite.lua", module = "sqlite" },
 	},
 	config = function()
-		require("neoclip").setup()
-
+		require("neoclip").setup({
+			enable_persistent_history = true,
+		})
 
 		vim.keymap.set("n", "<leader>sc", "<CMD>Telescope neoclip<CR>", { desc = "clippings" })
 	end,
