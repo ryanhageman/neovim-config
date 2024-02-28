d5c04adb127f40ecc465af92e865047820604a6f
Ryan Hageman <ryan.hageman@teamsparq.com>
2024-02-28 14:10:49 -0600

    Update todo-comments
    Works outside of comments, for markdown
    Add keymaps

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,9 +1,18 @@
 return {
-	"folke/todo-comments.nvim",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	config = function()
-		require("todo-comments").setup()
+  "folke/todo-comments.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    require("todo-comments").setup({
+      keywords = {
+        TASK = { icon = " ", color = "info", alt = { "[ ]" } },
+        QUESTION = { icon = " ", color = "info" },
+        WAITING = { icon = " ", color = "warning" },
+      },
+      highlight = {
+        comments_only = false,
+      },
+    })
 
-		vim.keymap.set("n", "<leader>pT", "<cmd>TodoTrouble<CR>", { desc = "project [T]odos" })
-	end,
+    vim.keymap.set("n", "<leader>pT", "<cmd>TodoTrouble<CR>", { desc = "project [T]odos" })
+  end,
 }
