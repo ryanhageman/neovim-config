788f04d45ceac5343ec3b650f1411d7ec453a083
Ryan Hageman <ryan.hageman@teamsparq.com>
2024-03-06 09:36:57 -0600

    Add a snippet, update none-ls

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,23 +1,23 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
 
-		null_ls.setup({
-			sources = {
-				-- Lua --
-				null_ls.builtins.formatting.stylua,
+    null_ls.setup({
+      sources = {
+        -- Lua --
+        null_ls.builtins.formatting.stylua,
 
-				-- Ruby --
-				null_ls.builtins.diagnostics.rubocop,
-				null_ls.builtins.formatting.rubocop,
+        -- Ruby --
+        null_ls.builtins.diagnostics.rubocop,
+        null_ls.builtins.formatting.rubocop,
 
-				-- JavaScript --
-				null_ls.builtins.formatting.prettier,
+        -- JavaScript --
+        null_ls.builtins.formatting.prettier,
 
-				-- Python --
-				null_ls.builtins.formatting.black,
-			},
-		})
-	end,
+        -- Python --
+        null_ls.builtins.formatting.black,
+      },
+    })
+  end,
 }
--- snippets/markdown.lua
+++ snippets/markdown.lua
@@ -6,17 +6,25 @@ local f = ls.function_node
 local fmta = require("luasnip.extras.fmt").fmta
 
 return {
-  -- Log Entry --
-  s(
-    { trig = "log", desc = "h2 with a timestamp" },
-    fmta("## <timestamp> - <title>", {
-      timestamp = f(function()
-        return os.date("%I:%M %p")
-      end, {}),
-      title = i(0),
-    })
-  ),
+	-- Log Entry --
+	s(
+		{ trig = "log", desc = "h2 with a timestamp" },
+		fmta("## <timestamp> - <title>", {
+			timestamp = f(function()
+				return os.date("%I:%M %p")
+			end, {}),
+			title = i(0),
+		})
+	),
 
-  -- Check Box --
-  s({ trig = "xchk", desc = "markdown checkbox" }, t("- [ ] ")),
+	-- Check Box --
+	s({ trig = "xchk", desc = "markdown checkbox" }, t("- [ ] ")),
+
+	-- Wiki Link --
+	s(
+		{ trig = "wiki", desc = "add a wiki link" },
+		fmta("[[<link_name>]]", {
+			link_name = i(1),
+		})
+	),
 }
