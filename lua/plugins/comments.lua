887b28cbeabb236b25eb917d5e662d8ac48473dc
ryanhageman <ryanhageman@icloud.com>
2024-03-16 11:35:44 -0500

    Add comment box keymaps

--- lua/plugins/comments.lua
+++ lua/plugins/comments.lua
@@ -1,13 +1,25 @@
 return {
-  -- Commenting --
-  {
-    "numToStr/Comment.nvim",
-    opts = {},
-    lazy = false,
-  },
+	-- Commenting --
+	{
+		"numToStr/Comment.nvim",
+		opts = {},
+		lazy = false,
+	},
 
-  -- Comments Based on the current line --
-  {
-    "JoosepAlviste/nvim-ts-context-commentstring",
-  },
+	-- Comments Based on the current line --
+	{
+		"JoosepAlviste/nvim-ts-context-commentstring",
+	},
+
+	-- Comment Box --
+	{
+		"LudoPinelli/comment-box.nvim",
+		config = function()
+			vim.keymap.set({ "n", "v" }, "<leader>ccb", "<cmd>CBlcbox<CR>", { desc = "comment box" })
+			vim.keymap.set({ "n", "v" }, "<leader>ccl", "<cmd>CBlcline<CR>", { desc = "comment line" })
+			vim.keymap.set({ "n", "v" }, "<leader>ccd", "<cmd>CBline<CR>", { desc = "divider line" })
+			vim.keymap.set({ "n", "v" }, "<leader>ccs", "<cmd>CBllline2<CR>", { desc = "start curved line" })
+			vim.keymap.set({ "n", "v" }, "<leader>cce", "<cmd>CBllline3<CR>", { desc = "end curved line" })
+		end,
+	},
 }
