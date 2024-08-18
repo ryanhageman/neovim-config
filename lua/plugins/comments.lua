7ad60e7c238aba1b6ee96990e98ed712ad9effe6
Ryan Hageman <ryanhageman@icloud.com>
2024-08-18 11:58:38 -0500

    feat: add new comment line option

--- lua/plugins/comments.lua
+++ lua/plugins/comments.lua
@@ -17,6 +17,7 @@ return {
 		config = function()
 			vim.keymap.set({ "n", "v" }, "<leader>ccb", "<cmd>CBlcbox<CR>", { desc = "comment box" })
 			vim.keymap.set({ "n", "v" }, "<leader>ccl", "<cmd>CBlcline<CR>", { desc = "comment line" })
+			vim.keymap.set({ "n", "v" }, "<leader>ccL", "<cmd>CBllline<CR>", { desc = "comment line (Left)" })
 			vim.keymap.set({ "n", "v" }, "<leader>ccd", "<cmd>CBline<CR>", { desc = "divider line" })
 			vim.keymap.set({ "n", "v" }, "<leader>ccs", "<cmd>CBllline2<CR>", { desc = "start curved line" })
 			vim.keymap.set({ "n", "v" }, "<leader>cce", "<cmd>CBllline3<CR>", { desc = "end curved line" })
