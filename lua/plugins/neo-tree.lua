95e7ed778fdcaa9cce5d061d9861f3db1ad519d9
Ryan Hageman <ryanhageman@icloud.com>
2024-09-02 18:38:56 -0500

    Revert "feat: add neo-tree buffer explorer keymap"
    This reverts commit a421eb9bb207e77c01ec202ceb384136d7d88a11.

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,20 @@
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require("neo-tree").setup({
+			filesystem = {
+				filtered_items = {
+					hide_dotfiles = false,
+					hide_gitignored = false,
+				},
+			},
+		})
+		vim.keymap.set("n", "<leader>e", ":Neotree filesystem toggle reveal_force_cwd<CR>", { desc = "[e]xplorer" })
+	end,
+}
