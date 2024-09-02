052c7fcdc2ec44081bb59892bf30c4c1ab372916
Ryan Hageman <ryanhageman@icloud.com>
2024-09-02 10:34:09 -0500

    feat: turn on buffer bookmarks in arrow
    Lets you set, view, and jump around to bookmarks in each buffer.

--- lua/plugins/arrow.lua
+++ lua/plugins/arrow.lua
@@ -1,7 +1,8 @@
 return {
-  "otavioschwanck/arrow.nvim",
-  opts = {
-    show_icons = true,
-    leader_key = ',' -- Recommended to be a single key
-  }
+	"otavioschwanck/arrow.nvim",
+	opts = {
+		show_icons = true,
+		leader_key = ",", -- Recommended to be a single key
+		buffer_leader_key = "m", -- Buffer bookmarks
+	},
 }
