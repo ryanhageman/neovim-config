969a5b4f17086eaa60aa55e558ea29e1bcc33418
Ryan Hageman <ryanhageman@icloud.com>
2024-12-28 19:19:58 -0600

    refactor: modularize splash ascii art for dashboard

---
+++ lua/plugins/dashboard/splash.lua
@@ -0,0 +1,7 @@
+local M = {}
+
+M.dont_panic = require("plugins.dashboard.dont_panic")
+M.jolly_roger = require("plugins.dashboard.jolly_roger")
+M.scooby_doo = require("plugins.dashboard.scooby_doo")
+
+return M
