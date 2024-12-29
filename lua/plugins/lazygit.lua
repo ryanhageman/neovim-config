750d7bffc0daf6f40d38528c0c1fa46e1b8e6600
Ryan Hageman <ryanhageman@icloud.com>
2024-12-29 14:48:37 -0600

    build: remove lazygit plugin
    Handled by Snacks now

--- lua/plugins/lazygit.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"kdheepak/lazygit.nvim",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		config = function()
-			vim.keymap.set("n", "<leader>gl", "<cmd>LazyGit<CR>", { desc = "LazyGit" })
-		end,
-	},
-}
