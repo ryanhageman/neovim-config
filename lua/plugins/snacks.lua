2f91a7fe73b1bec1055515192fd58ad4c98b05e5
Ryan Hageman <ryanhageman@icloud.com>
2024-12-29 14:24:43 -0600

    feat: add keymaps for Snacks Dim

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -4,11 +4,8 @@ return {
 	"folke/snacks.nvim",
 	priority = 1000,
 	lazy = false,
-	---@type snacks.Config
+
 	opts = {
-		-- your configuration comes here
-		-- or leave it empty to use the default settings
-		-- refer to the configuration section below
 		bigfile = { enabled = true },
 		dashboard = {
 			enabled = true,
@@ -23,6 +20,7 @@ return {
 				},
 			},
 		},
+		dim = {},
 		indent = { enabled = true },
 		input = { enabled = true },
 		notifier = { enabled = true },
@@ -30,5 +28,10 @@ return {
 		scroll = { enabled = true },
 		statuscolumn = { enabled = true },
 		words = { enabled = true },
+	},
+
+	keys = {
+		{ "<leader>bzd", function() Snacks.dim() end, desc = "Dim", },
+		{ "<leader>bzu", function() Snacks.dim.disable() end, desc = "unDim", },
 	},
 }
