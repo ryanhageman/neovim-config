eccc067b11fd2064124c7cbdcb05ea64784f7787
Ryan Hageman <ryanhageman@icloud.com>
2024-12-29 14:31:25 -0600

    feat: add Snacks GitBrowse keymap

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -33,6 +33,7 @@ return {
 	keys = {
 		{ "<leader>bzd", function() Snacks.dim() end, desc = "Dim", },
 		{ "<leader>bzu", function() Snacks.dim.disable() end, desc = "unDim", },
+    { "<leader>gB", function() Snacks.gitbrowse() end, desc = "Git Browse", mode = { "n", "v" } },
     { "<leader>gb", function() Snacks.git.blame_line() end, desc = "Git Blame Line" },
 	},
 }
