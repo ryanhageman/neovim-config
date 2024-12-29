36f886561d4cc5b1250e519227c8d2c3743d9f31
Ryan Hageman <ryanhageman@icloud.com>
2024-12-28 19:19:37 -0600

    refactor: autoformat css ls file

--- lua/plugins/lsp/css.lua
+++ lua/plugins/lsp/css.lua
@@ -1,50 +1,50 @@
 local M = {}
-  local capabilities = require("cmp_nvim_lsp").default_capabilities()
-  local lspconfig = require("lspconfig")
-  local navic = require("nvim-navic")
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+local lspconfig = require("lspconfig")
+local navic = require("nvim-navic")
 
 function M.cssls_setup()
-  -- local capabilities = require("cmp_nvim_lsp").default_capabilities()
-  -- local lspconfig = require("lspconfig")
-  -- local navic = require("nvim-navic")
+	-- local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	-- local lspconfig = require("lspconfig")
+	-- local navic = require("nvim-navic")
 
-  lspconfig.cssls.setup({
-    capabilities = capabilities,
-    on_attach = function(client, bufnr)
-      navic.attach(client, bufnr)
-    end,
-  })
+	lspconfig.cssls.setup({
+		capabilities = capabilities,
+		on_attach = function(client, bufnr)
+			navic.attach(client, bufnr)
+		end,
+	})
 end
 
 function M.css_variables_setup()
-  lspconfig.css_variables.setup({
-    settings = {
-      cssVariables = {
-        blacklistFolders = {
-          "**/.cache",
-          "**/.DS_Store",
-          "**/.git",
-          "**/.hg",
-          "**/.next",
-          "**/.svn",
-          "**/bower_components",
-          "**/CVS",
-          "**/dist",
-          "**/node_modules",
-          "**/tests",
-          "**/tmp",
-          "**/Library/**",
-          "**/Documents/**",
-          "**/Downloads/**",
-          "**/Desktop/**",
-          "**/Dropbox/**",
-          "**/Google Drive/**",
-          "**/iCloud Drive/**",
-          "**/.Trash/**",
-        },
-      },
-    },
-  })
+	lspconfig.css_variables.setup({
+		settings = {
+			cssVariables = {
+				blacklistFolders = {
+					"**/.cache",
+					"**/.DS_Store",
+					"**/.git",
+					"**/.hg",
+					"**/.next",
+					"**/.svn",
+					"**/bower_components",
+					"**/CVS",
+					"**/dist",
+					"**/node_modules",
+					"**/tests",
+					"**/tmp",
+					"**/Library/**",
+					"**/Documents/**",
+					"**/Downloads/**",
+					"**/Desktop/**",
+					"**/Dropbox/**",
+					"**/Google Drive/**",
+					"**/iCloud Drive/**",
+					"**/.Trash/**",
+				},
+			},
+		},
+	})
 end
 
 return M
