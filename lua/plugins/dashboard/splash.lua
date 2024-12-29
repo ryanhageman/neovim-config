f1ef76ce18e150b6b51ef8a7896692b572479b8e
Ryan Hageman <ryanhageman@icloud.com>
2024-12-29 14:23:52 -0600

    fix: rendering of dashboard splah images

--- lua/plugins/dashboard/splash.lua
+++ lua/plugins/dashboard/splash.lua
@@ -1,7 +1,7 @@
-local M = {}
-
-M.dont_panic = require("plugins.dashboard.dont_panic")
-M.jolly_roger = require("plugins.dashboard.jolly_roger")
-M.scooby_doo = require("plugins.dashboard.scooby_doo")
+local M = {
+	dont_panic = require("plugins.dashboard.dont_panic"),
+	jolly_roger = require("plugins.dashboard.jolly_roger"),
+	scooby_doo = require("plugins.dashboard.scooby_doo"),
+}
 
 return M
