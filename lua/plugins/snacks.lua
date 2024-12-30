cf0303daed68c67deb7e7ebceda44c113a4a235e
Ryan Hageman <ryanhageman@icloud.com>
2024-12-29 20:14:40 -0600

    feat: Snacks add toggles and cleanup

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -28,13 +28,10 @@ return {
     scope = {enabled = true },
 		scroll = { enabled = true },
 		statuscolumn = { enabled = true },
-    terminal = { enabled = true },
 		words = { enabled = true },
 	},
 
 	keys = {
-		{ "<leader>bzd", function() Snacks.dim() end, desc = "Dim", },
-		{ "<leader>bzu", function() Snacks.dim.disable() end, desc = "unDim", },
     { "<leader>gB", function() Snacks.gitbrowse() end, desc = "Git Browse", mode = { "n", "v" } },
     { "<leader>gb", function() Snacks.git.blame_line() end, desc = "Git Blame Line" },
     { "<leader>gf", function() Snacks.lazygit.log_file() end, desc = "Lazygit Current File History" },
@@ -44,5 +41,36 @@ return {
     { "<leader>uh",  function() Snacks.notifier.show_history() end, desc = "Notification History" },
     { "<leader>,",  function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
     { "<leader>bS",  function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
+    { "<leader>bzz",  function() Snacks.zen() end, desc = "Toggle Zen Mode" },
+    { "<leader>bzZ",  function() Snacks.zen.zoom() end, desc = "Toggle Zoom" }, 
 	},
+
+  init = function()
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "VeryLazy",
+      callback = function()
+        -- Setup some globals for debugging (lazy-loaded)
+        _G.dd = function(...)
+          Snacks.debug.inspect(...)
+        end
+        _G.bt = function()
+          Snacks.debug.backtrace()
+        end
+        vim.print = _G.dd -- Override print to use snacks for `:=` command
+
+        -- Create some toggle mappings
+        Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
+        Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
+        Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
+        Snacks.toggle.diagnostics():map("<leader>ud")
+        Snacks.toggle.line_number():map("<leader>ul")
+        Snacks.toggle.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map("<leader>uc")
+        Snacks.toggle.treesitter():map("<leader>uT")
+        Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map("<leader>ub")
+        Snacks.toggle.inlay_hints():map("<leader>uh")
+        Snacks.toggle.indent():map("<leader>ug")
+        Snacks.toggle.dim():map("<leader>uD")
+      end,
+    })
+  end,
 }
